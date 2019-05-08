f36e23b9f67a7fe29d6b960f763dce9ca9916a41
Vipin Mahajan <vipin.mahajan@redbots.de>
2019-05-08 17:11:32 +0200

    for dele

--- src/CleanupProfileverwaltung/For_deletion.java
+++ src/CleanupProfileverwaltung/For_deletion.java
@@ -1,5 +1,11 @@
 package CleanupProfileverwaltung;
 
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import CleanupProfileverwaltung.cleanup_Profile;
+import CleanupProfileverwaltung.*;
 
 public class For_deletion {
 //for deletion
